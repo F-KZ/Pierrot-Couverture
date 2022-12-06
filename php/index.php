713385d4359be386cab2d4beb3cbafb1785577d3
F-KZ <supreme-soul@hotmail.fr>
2022-12-06 16:34:12 +0100

    jpeg

--- php/index.php
+++ php/index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mail</title>
-</head>
-<body>
-    <?php
+<?php
     ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
     $from = "contact@pierre-couverture-france.fr";
@@ -18,5 +9,15 @@
     mail($to,$subject,$message, $headers);
     echo "L'email a été envoyé.";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mail</title>
+</head>
+<body>
+   
 </body>
 </html>
