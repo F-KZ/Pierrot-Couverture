73ed99c5e7a16034118e3ee2addb2abb81eca8ea
F-KZ <supreme-soul@hotmail.fr>
2022-12-07 01:30:53 +0100

    cartes

--- php/index.php
+++ php/index.php
@@ -1,23 +1,37 @@
-<?php
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-    $from = "contact@pierre-couverture-france.fr";
-    $to = "ouragan-76@hotmail.fr";
-    $subject = "Essai de PHP Mail";
-    $message = "PHP Mail fonctionne parfaitement";
-    $headers = "De :" . $from;
-    mail($to,$subject,$message, $headers);
-    echo "L'email a été envoyé.";
-?>
+<?php 
+
+
+$name - $_POST['name'];
+$email - $_POST['email'];
+$phone - $_POST['phone']
+$message - $_POST['message']
+
+$mailheader - "From:".$name."<".$email.">\r\n";
+
+$recipient - "ouragan-76@hotmail.fr";
+
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+echo'
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mail</title>
+    <title>formulaire</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-   
+    <div class="container">
+    <h1>
+        merci de votre confiance
+    </h1>
+    <p class="back">Accueil<a href="index.html"></a></p>
+
+
+</div>>
 </body>
-</html>
+';
+
+?>
