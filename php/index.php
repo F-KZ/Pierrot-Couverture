bedfd393fc7ab25f3bac27514b3b952849e90d6a
F-KZ <supreme-soul@hotmail.fr>
2022-12-11 03:26:50 +0100

    message de confirmation

--- php/index.php
+++ php/index.php
@@ -1,37 +1,40 @@
-<?php 
+<?php
 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
 
-$name - $_POST['name'];
-$email - $_POST['email'];
-$phone - $_POST['phone']
-$message - $_POST['message']
+$mailheader = "From:".$name."<".$email.">\r\n";
 
-$mailheader - "From:".$name."<".$email.">\r\n";
+$recipient = "oura6@hotmail.fr";
 
-$recipient - "ouragan-76@hotmail.fr";
-
-mail($recipient, $subject, $message, $mailheader) or die("Error!");
+mail($recipient, $phone, $message, $mailheader) or die("Error!");
 
 echo'
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>formulaire</title>
-    <link rel="stylesheet" href="./style.css">
+    <title>Contact form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
-    <h1>
-        merci de votre confiance
-    </h1>
-    <p class="back">Accueil<a href="index.html"></a></p>
-
-
-</div>>
+        <h1>Thank you for contacting me. I will get back to you as soon as possible!</h1>
+        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
+        
+    </div>
 </body>
+</html>
+
+
+
 ';
+
 
 ?>
